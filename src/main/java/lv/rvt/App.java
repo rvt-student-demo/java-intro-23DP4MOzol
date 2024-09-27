45aa53fed408ac70e69cd2a4c6f66e0056ba0e4e
23DP4MOzol <166484750+23DP4MOzol@users.noreply.github.com>
2024-09-27 09:37:56 +0000

    Adulthood

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -80,6 +80,17 @@ public class App
         // scanner.close();
 
 
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Give a number :");
+        // int number = Integer.valueOf(scanner.nextLine());
+        // if (number >= 18) {
+        //      System.out.println("You are an adult");
+        // } else {
+        //     System.out.println("You are not an adult");
+        // }
+        // scanner.close();
+
+
 
 
 
