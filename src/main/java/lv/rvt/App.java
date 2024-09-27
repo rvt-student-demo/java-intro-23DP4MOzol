2d409f5699b85ed58f2bc9667d8239596d0d3d33
23DP4MOzol <166484750+23DP4MOzol@users.noreply.github.com>
2024-09-27 09:38:48 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -105,7 +105,30 @@ public class App
         // }
         // scanner.close();
         
-
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Give points [0-100]: ");
+        int points = scanner.nextInt();
+        String grade;
+        if (points < 0) {
+            grade = "impossible!";
+        } else if (points <= 49) {
+            grade = "failed";
+        } else if (points <= 59) {
+            grade = "1";
+        } else if (points <= 69) {
+            grade = "2";
+        } else if (points <= 79) {
+            grade = "3";
+        } else if (points <= 89) {
+            grade = "4";
+        } else if (points <= 100) {
+            grade = "5";
+        } else {
+            grade = "incredible!";
+        }
+        System.out.println("Grade: " + grade);
+        scanner.close();
 
 
 
