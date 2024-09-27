ffd46411f8de4bff90124baef80270ce0edc0088
23DP4MOzol <166484750+23DP4MOzol@users.noreply.github.com>
2024-09-27 10:48:11 +0000

    Gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -178,15 +178,41 @@ public class App
         // scanner.close();
 
 
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Give a year: ");
+        // int year = scanner.nextInt();
+        // if (year % 4 != 0) {
+        //     System.out.println("The year is not a leap year.");
+        // } else if (year % 100 == 0 && year % 400 != 0) {
+        //     System.out.println("The year is not a leap year.");
+        // } else {
+        //     System.out.println("The year is a leap year.");
+        // }
+        // scanner.close();
+
+
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Give a year: ");
-        int year = scanner.nextInt();
-        if (year % 4 != 0) {
-            System.out.println("The year is not a leap year.");
-        } else if (year % 100 == 0 && year % 400 != 0) {
-            System.out.println("The year is not a leap year.");
+        System.out.print("Value of the gift? ");
+        int giftValue = scanner.nextInt();
+        double tax = 0.0;
+        if (giftValue < 5000) {
+            System.out.println("No tax!");
+        } else if (giftValue <= 25000) {
+            tax = 100 + (giftValue - 5000) * 0.08;
+            System.out.println("Tax: " + tax);
+        } else if (giftValue <= 55000) {
+            tax = 1700 + (giftValue - 25000) * 0.10;
+            System.out.println("Tax: " + tax);
+        } else if (giftValue <= 200000) {
+            tax = 4700 + (giftValue - 55000) * 0.12;
+            System.out.println("Tax: " + tax);
+        } else if (giftValue <= 1000000) {
+            tax = 22100 + (giftValue - 200000) * 0.15;
+            System.out.println("Tax: " + tax);
         } else {
-            System.out.println("The year is a leap year.");
+            tax = 142100 + (giftValue - 1000000) * 0.17;
+            System.out.println("Tax: " + tax);
         }
         scanner.close();
     
