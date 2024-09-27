ea472489ad368ac7468ec66bb4200c8d669097ea
23DP4MOzol <166484750+23DP4MOzol@users.noreply.github.com>
2024-09-27 09:35:48 +0000

    Speeding Ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -11,12 +11,14 @@ public class App
         // String valueAsString = "42.42";
         // double value = Double.valueOf(valueAsString);
         // System.out.println(value);
+        // scanner.close();
 
 
         // Scanner Scanner = new Scanner(System.in);
         // System.out.println("Write a boolean ");
         // boolean value = Boolean.valueOf(scanner.nextLine());
         // System.out.println("You wrote " + value);
+        // scanner.close();
 
 
 
@@ -25,22 +27,37 @@ public class App
         // int integer = Integer.valueOf(scanner.nextLine());
         // double floatingPoint = Double.valueOf(scanner.nextLine());
         // boolean trueOrFalse = Boolean.valueOf(scanner.nextLine());
+        // scanner.close();
 
-        Scanner scanner = new Scanner(System.in);
+        // Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give the first number:");
-        int first = Integer.valueOf(scanner.nextLine());
-        System.out.println("Give the second number:");
-        int second = Integer.valueOf(scanner.nextLine());
+        // System.out.println("Give the first number:");
+        // int first = Integer.valueOf(scanner.nextLine());
+        // System.out.println("Give the second number:");
+        // int second = Integer.valueOf(scanner.nextLine());
 
-        System.out.println(first + " + " + second + " = " + (first + second));
+        // System.out.println(first + " + " + second + " = " + (first + second));
 
-        System.out.println(first + " - " + second + " = " + (first - second));
+        // System.out.println(first + " - " + second + " = " + (first - second));
 
-        System.out.println(first + " * " + second + " = " + (first * second));
+        // System.out.println(first + " * " + second + " = " + (first * second));
         
-        double result = first / (double) second;  
-        System.out.println(first + " / " + second + " = " + result);
+        // double result = first / (double) second;  
+        // System.out.println(first + " / " + second + " = " + result);
+        // scanner.close();
+
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Give speed");
+        // int number = Integer.valueOf(scanner.nextLine());
+        // if (number > 120) {
+        //      System.out.println("Speeding ticket!");
+        // scanner.close();
+
+
+
+
+
+}
 
     }
-}
