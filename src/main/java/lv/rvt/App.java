53f351f31b09f6d1829fa4f096ee43a6f21afd85
23DP4MOzol <166484750+23DP4MOzol@users.noreply.github.com>
2024-10-10 10:49:11 +0000

    Only positives

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,32 @@
 package lv.rvt;
 
-import java.util.*;
+import java.util.Scanner;
+
+// import java.util.*;
 
 public class App 
 {
     public static void main( String[] args )
     {
+
+        // Scanner scanner = new Scanner(System.in);
+
+        // int numbersRead = 0;
+        // int sum = 0;
+
+        // while (true) {
+        //     if (numbersRead == 5) {
+        //         break;
+        //     }
+
+        //     System.out.println("Input number");
+        //     sum = sum + Integer.valueOf(scanner.nextLine());
+        //     numbersRead = numbersRead + 1;
+        // }
+
+        // System.out.println("The sum of the numbers is " + sum);
+        // scanner.close();
+
         // Scanner scanner = new Scanner(System.in);
 
         // String valueAsString = "42.42";
@@ -192,29 +213,51 @@ public class App
 
 
 
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Value of the gift? ");
+        // int giftValue = scanner.nextInt();
+        // double tax = 0.0;
+        // if (giftValue < 5000) {
+        //     System.out.println("No tax!");
+        // } else if (giftValue <= 25000) {
+        //     tax = 100 + (giftValue - 5000) * 0.08;
+        //     System.out.println("Tax: " + tax);
+        // } else if (giftValue <= 55000) {
+        //     tax = 1700 + (giftValue - 25000) * 0.10;
+        //     System.out.println("Tax: " + tax);
+        // } else if (giftValue <= 200000) {
+        //     tax = 4700 + (giftValue - 55000) * 0.12;
+        //     System.out.println("Tax: " + tax);
+        // } else if (giftValue <= 1000000) {
+        //     tax = 22100 + (giftValue - 200000) * 0.15;
+        //     System.out.println("Tax: " + tax);
+        // } else {
+        //     tax = 142100 + (giftValue - 1000000) * 0.17;
+        //     System.out.println("Tax: " + tax);
+        // }
+        // scanner.close(); 
+
+
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Value of the gift? ");
-        int giftValue = scanner.nextInt();
-        double tax = 0.0;
-        if (giftValue < 5000) {
-            System.out.println("No tax!");
-        } else if (giftValue <= 25000) {
-            tax = 100 + (giftValue - 5000) * 0.08;
-            System.out.println("Tax: " + tax);
-        } else if (giftValue <= 55000) {
-            tax = 1700 + (giftValue - 25000) * 0.10;
-            System.out.println("Tax: " + tax);
-        } else if (giftValue <= 200000) {
-            tax = 4700 + (giftValue - 55000) * 0.12;
-            System.out.println("Tax: " + tax);
-        } else if (giftValue <= 1000000) {
-            tax = 22100 + (giftValue - 200000) * 0.15;
-            System.out.println("Tax: " + tax);
-        } else {
-            tax = 142100 + (giftValue - 1000000) * 0.17;
-            System.out.println("Tax: " + tax);
+        while (true) {
+            System.out.print("Give a number: ");
+            int number = scanner.nextInt();
+
+            if (number < 0) {
+                System.out.println("Unsuitable number");
+            } else if (number == 0) {
+                break;
+            } else {
+                System.out.println(number * number);
+            }
         }
         scanner.close();
+
+
+
+
+        
     
 }
 
