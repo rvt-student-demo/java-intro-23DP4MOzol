390358f5d7e2ba2b4ca68f17226abcc074426f8d
23DP4MOzol <166484750+23DP4MOzol@users.noreply.github.com>
2024-10-31 12:06:39 +0000

    Two Arrays

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -436,19 +436,38 @@ package lv.rvt;
 // } 
 
 
- public class App 
- {
-      public static void main( String[] args )
+//  public class App 
+//  {
+//       public static void main( String[] args )
+//     {
+//     int[] val = {0, 1, 2, 3}; 
+//     int sum = 0; 
+
+//     for (int i = 0; i < val.length; i++) {
+//       sum += val[i];
+//     }
+
+//     System.out.println( "Sum of all numbers = " + sum );
+//   }
+// }
+public class App 
+{
+    public static void main ( String[] args )
     {
-    int[] val = {0, 1, 2, 3}; 
-    int sum = 0; 
+        int[] val = {13, -4, 82, 17}; 
+        int[] twice = new int[val.length]; 
 
-    for (int i = 0; i < val.length; i++) {
-      sum += val[i];
-    }
+        System.out.println("Original Array: " 
+            + val[0] + " " + val[1] + " " + val[2] + " " + val[3]);
 
-    System.out.println( "Sum of all numbers = " + sum );
-  }
+
+        for (int i = 0; i < val.length; i++) {
+            twice[i] = val[i] * 2;
+        }
+
+        System.out.println("New Array: " 
+            + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3]);
+        }
 }
 
     
